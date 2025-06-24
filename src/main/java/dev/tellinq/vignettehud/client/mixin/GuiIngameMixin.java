f3c710736c4e63cb5ad0126dbd58915f738a08bb
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-06-24 07:54:30 -0400

    Remove import

--- src/main/java/dev/tellinq/vignettehud/client/mixin/GuiIngameMixin.java
+++ src/main/java/dev/tellinq/vignettehud/client/mixin/GuiIngameMixin.java
@@ -5,7 +5,6 @@ import dev.tellinq.vignettehud.client.VignetteHUDConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(GuiIngame.class)
 public class GuiIngameMixin {
